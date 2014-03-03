56b53a78daa772a6551bcb9c4b2c66509508548c
Lucas Waye <lwaye@fas.harvard.edu>
2014-03-03 10:07:18 -0500

    cleanup

--- vr.erl
+++ vr.erl
@@ -2,19 +2,15 @@
 -export([start/0, init/1, masterAwaitingRequest/2, replicaAwaitingPrepare/2, terminate/3, stop/0, handle_event/3]).
 -behavior(gen_fsm).
 
-init([Master]) when Master == youAre ->
-    io:fwrite("i am master\n"),
-    {ok, masterAwaitingRequest, {[], self(), dict:new(), 0, 0, 0}};
-init([Master]) ->
-    %io:fwrite("~p i am ~s", [self(), Master]),
-    {ok, replicaAwaitingPrepare, {[], Master, dict:new(), 0, 0, 0}}.
+init({State, Master}) ->
+    {ok, State, {[], Master, dict:new(), 0, 0, 0}}.
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% REPLICA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 replicaAwaitingPrepare({prepare, ViewNumber, Op, NewOpNumber, CommitNumber}, 
     State = {Log, Master, ClientsTable, OpNumber, CommitNumber, ViewNumber}) ->
-    io:fwrite("got prepare!\n"),
+    io:fwrite("got prepare!~n"),
     {stop, normal, State}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% MASTER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -22,24 +18,24 @@ replicaAwaitingPrepare({prepare, ViewNumber, Op, NewOpNumber, CommitNumber},
 
 broadcastToReplicas(Master,Message) ->
     lists:map(
-      fun(Replica) ->
-            io:fwrite("hello"),
-              gen_fsm:send_event(Replica,Message)
-      end,
-      [Node || Node <- all_nodes(), Node =/= Master]).
+        fun(Replica) ->
+            io:fwrite("sending to replica ~p~n", [Replica]),
+            gen_fsm:send_event(Replica,Message)
+        end,
+        [Node || Node <- all_nodes(), Node =/= Master]).
 
 masterAwaitingRequest({request, Op, Client, RequestNum}, State = {Log, Master, ClientsTable, OpNumber, CommitNumber, ViewNumber}) ->
-    io:fwrite("in master awaiting request\n"),
+    io:fwrite("in master awaiting request~n"),
     case dict:find(Client, ClientsTable) of
         {ok, N} when RequestNum < N -> 
-            io:fwrite("drop\n"), {next_state, masterAwaitingRequest, State};
+            io:fwrite("drop old request~n"), {next_state, masterAwaitingRequest, State};
         {ok, {N, unknown}} when RequestNum == N -> 
-            io:fwrite("drop-still working on it\n"), {next_state, masterAwaitingRequest, State};
+            io:fwrite("drop-still working on it~n"), {next_state, masterAwaitingRequest, State};
         {ok, {N, LastValue}} when RequestNum == N -> 
             % resend(), XXX FIXME TODO
             {next_state, masterAwaitingRequest, State};
         _ -> 
-            io:fwrite("new request ~p by ~p", [RequestNum, Client]),
+            io:fwrite("new request ~p by ~p~n", [RequestNum, Client]),
             NewClientsTable = dict:store(Client, {RequestNum, unknown}, ClientsTable),
             NewLog = [Op|Log],
             NewOpNumber = OpNumber + 1,
@@ -54,14 +50,14 @@ handle_event(stop, _, State) ->
     {stop, normal, State}.
 
 terminate(_,_,_) ->
-    io:fwrite("terminating!\n").
+    io:fwrite("terminating!~n").
 
 all_nodes() ->
     [vr, vr2].
 
 start() -> 
-    MasterP = gen_fsm:start_link({local, vr}, vr, [youAre], []),
-    gen_fsm:start_link({local, vr2}, vr, [MasterP], []),
+    gen_fsm:start_link({local, vr}, vr, {masterAwaitingRequest, vr}, []),
+    gen_fsm:start_link({local, vr2}, vr, {replicaAwaitingPrepare, vr}, []),
     gen_fsm:send_event(vr, {request, hello, 1, 1}).
 
 
