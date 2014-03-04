54109749f78842a109b1bd6a1eca9b43a1ebe260
Lucas Waye <wayetender@gmail.com>
2014-03-03 19:01:30 -0500

    checkpoint, getting close to removing old vr

--- vr.erl
+++ vr.erl
@@ -3,23 +3,26 @@
 -behavior(gen_fsm).
 
 init({State, Master, ReplicaCommitFn}) ->
-    {ok, State, {[], Master, dict:new(), 0, 0, 0, ReplicaCommitFn}}.
+    {ok, State, {[], Master, dict:new(), 0, 0, 0, ReplicaCommitFn, noone}}.
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% REPLICA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 replicaAwaitingPrepare({prepare, MasterViewNumber, Op, MasterOpNumber, MasterCommitNumber}, 
-    State = {Log, Master, ClientsTable, OpNumber, CommitNumber, ViewNumber, ReplicaCommitFn}) ->
+    State = {Log, Master, ClientsTable, OpNumber, CommitNumber, ViewNumber, ReplicaCommitFn, From}) ->
     io:fwrite("got prepare~n"),
     if 
         (MasterCommitNumber == CommitNumber) and (MasterOpNumber == OpNumber + 1)  ->
             io:fwrite("good prepare ~n"),
             NewOpNumber = MasterOpNumber,
-            NewCommitNumber = CommitNumber;
+            NewCommitNumber = CommitNumber,
+            NewLog = [Op|Log],
+            NewClientsTable = dict:store(Client, {RequestNum, unknown}, ClientsTable),
+            sendToMaster(Master, {prepareOk, ViewNumber, NewOpNumber}),
+            {next_state, replicaAwaitingPrepare, {NewLog, Master, NewClientsTable, OpNumber, CommitNumber, ViewNumber, ReplicaCommitFn, From}}
         (MasterCommitNumber > CommitNumber) and (MasterCommitNumber > OpNumber) ->
-            io:fwrite("initiate state transfer~n"),
-            NewOpNumber = OpNumber,
-            NewCommitNumber = CommitNumber;
+            io:fwrite("out of sync with commit numbers; initiate state transfer~n"),
+            {next_state, replicaStateTransfer, {Log, Master, ClientsTable, OpNumber, CommitNumber, ViewNumber, ReplicaCommitFn, From}}
         (MasterCommitNumber > CommitNumber) and (MasterCommitNumber == OpNumber) ->
             io:fwrite("Ops ~s to ~s got committed ~n", [CommitNumber, MasterCommitNumber]),
             % commit them
@@ -29,13 +32,11 @@ replicaAwaitingPrepare({prepare, MasterViewNumber, Op, MasterOpNumber, MasterCom
             io:fwrite("invariant failure: ~s~n", {OpNumber, CommitNumber, MasterOpNumber, MasterCommitNumber}),
             erlang:error(badReplicaPrepareInvariant)
     end,
-    sendToMaster(Master, {prepareOk, Op}),
-    {stop, normal, State}.
+    {stop, normal, State};
 
-
-replicaAwaitingPrepare({prepare, MasterViewNumber, Op, MasterOpNumber, MasterCommitNumber}, 
-    State = {Log, Master, ClientsTable, OpNumber, CommitNumber, ViewNumber}) ->
-    
+replicaAwaitingPrepare({request, Client, _, _, From}, State = {_, Master, _, _, _, _, _, _}) ->
+    From ! {notMaster, Master},
+    {next_state, replicaAwaitingPrepare, State}.
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% MASTER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -53,8 +54,14 @@ sendToMaster(Master,Message) ->
     io:fwrite("sending to master ~p~n", [Master]),
     gen_fsm:send_event(Master,Message).
 
-masterAwaitingRequest({request, Op, Client, RequestNum}, 
-    State = {Log, Master, ClientsTable, OpNumber, CommitNumber, ViewNumber, ReplicaCommitFn}) ->
+masterAwaitingRequest({committed, Client, RequestNum}, 
+    State = {Log, Master, ClientsTable, OpNumber, CommitNumber, ViewNumber, ReplicaCommitFn, From}) ->
+    io:fwrite("got committed operation"),
+    NewCommitNumber = CommitNumber + 1,
+    {next_state, masterAwaitingRequest, {Log, Master, ClientsTable, OpNumber, NewCommitNumber, ViewNumber, ReplicaCommitFn, From}};
+
+masterAwaitingRequest({request, Op, Client, RequestNum, From}, 
+    State = {Log, Master, ClientsTable, OpNumber, CommitNumber, ViewNumber, ReplicaCommitFn, _}) ->
     io:fwrite("in master awaiting request~n"),
     case dict:find(Client, ClientsTable) of
         {ok, N} when RequestNum < N -> 
@@ -70,8 +77,13 @@ masterAwaitingRequest({request, Op, Client, RequestNum},
             NewLog = [Op|Log],
             NewOpNumber = OpNumber + 1,
             broadcastToReplicas(Master, {prepare, ViewNumber, Op, NewOpNumber, CommitNumber}),
-            {next_state, masterAwaitingPrepareOks, {NewLog, Master, NewClientsTable, NewOpNumber, CommitNumber, ViewNumber, ReplicaCommitFn}}
+            {next_state, masterAwaitingPrepareOks, {NewLog, Master, NewClientsTable, NewOpNumber, CommitNumber, ViewNumber, ReplicaCommitFn, From}}
     end.
+
+masterAwaitingPrepareOks({prepareOk, Op, Client, RequestNum, From}, 
+    State = {Log, Master, ClientsTable, OpNumber, CommitNumber, ViewNumber, ReplicaCommitFn, _}) ->
+    io:fwrite("in master awaiting request~n"),
+
 
 stop() ->
     gen_fsm:send_all_state_event(vr, stop).
@@ -82,20 +94,35 @@ handle_event(stop, _, State) ->
 terminate(_,_,_) ->
     io:fwrite("terminating!~n").
 
-all_nodes() ->
-    [vrMaster, replica1].
-
 startMasterNode(FsmRef, CommitAsReplicaFun) ->
     gen_fsm:start_link({local, FsmRef}, vr, {masterAwaitingRequest, FsmRef, CommitAsReplicaFun}, []).
 
 startReplicaNode(FsmRef, Master, CommitAsReplicaFun) ->
     gen_fsm:start_link({local, FsmRef}, vr, {replicaAwaitingPrepare, Master, CommitAsReplicaFun}, []).
 
-handleClientRequest(FsmRef, Message = {Op, Client, RequestNum}) ->
-    gen_fsm:send_event(FsmRef, {request, Op, Client, RequestNum}).
+
+%% WRAPPER INTERFACE
+
+% not in the state machine
+handleClientRequest(FsmRef, Message = {Client, RequestNum, Op}) ->
+    % invariant precondition: in one of: masterAwaitingRequest, replicaAwaitingPrepare, replicaStateTransfer
+    gen_fsm:send_event(FsmRef, {request, Op, Client, RequestNum, self()}),
+    receive %readyToCommit or {notMaster, Master}
+        Response -> Response
+    end.
+
+registerMasterCommited(FsmRef, {Client, RequestNum, Result}) ->
+    % invariant: must be in masterAwaitingRequest
+    gen_fsm:send_event(FsmRef, {commited, Client, RequestNum}).
+
+
+%% Test code below
 
 commitAsReplicaTest(Msg) ->
     io:fwrite("committing as replica ~p~n", [Msg]).
+
+all_nodes() ->
+    [vrMaster, replica1].
 
 test() -> 
     Master = vrMaster,
--- vr2.erl
+++ vr2.erl
@@ -1,5 +1,5 @@
 -module(vr2).
--export([startNode/4, startPrepare/4, stop/0, test/0]).
+-export([startNode/4, startPrepare/4, stop/1, test/0]).
 -export([master/2, replica/2, handle_event/3, init/1, terminate/3]).
 -behavior(gen_fsm).
 
@@ -7,7 +7,9 @@ init({NodeStatus, State}) ->
     {ok, NodeStatus, State}.
 
 startNode(NodeName, MasterNode, AllNodes, CommitFn) ->
-    S = #{ commitFn => CommitFn, masterNode => MasterNode, allNodes => AllNodes },
+    S = #{ commitFn => CommitFn, masterNode => MasterNode, allNodes => AllNodes, 
+        clientsTable => dict:new(), viewNumber => 0, log => [], opNumber => 0, 
+        commitNumber => 0 },
     if 
         NodeName == MasterNode ->
             gen_fsm:start_link({local, NodeName}, vr2, {master, S}, []);
@@ -15,18 +17,62 @@ startNode(NodeName, MasterNode, AllNodes, CommitFn) ->
             gen_fsm:start_link({local, NodeName}, vr2, {replica, S}, [])
     end.
 
-master({clientRequest, Client, RequestNum, Op}, State) ->
+sendToClient(Client, Message) ->
+    Client ! Message.
+
+sendToReplicas(Master, Nodes, Message) ->
+    lists:map(
+        fun(Replica) ->
+            io:fwrite("sending ~p to replica ~p~n", [Message, Replica]),
+            gen_fsm:send_event(Replica, Message)
+        end,
+        [Node || Node <- Nodes, Node =/= Master]).
+
+
+master({clientRequest, Client, RequestNum, Op}, 
+    #{ clientsTable := ClientsTable, allNodes := AllNodes, masterNode := MasterNode, 
+        viewNumber := ViewNumber, log := Log, opNumber := OpNumber, 
+        commitNumber := CommitNumber } = State) ->
     io:fwrite("Got client request as master~n"),
-    {stop, normal, State}.
+    case dict:find(Client, ClientsTable) of
+        {ok, {N, _}} when RequestNum < N ->
+            io:fwrite("drop old request~n"),
+            {next_state, master, State};
+        {ok, {N, Res}} when (RequestNum == N) and (Res =/= inProgress) ->
+            io:fwrite("resending old result~n"),
+            sendToClient(Client, {RequestNum, Res}),
+            {next_state, master, State};
+        {ok, {N, Res}} when (RequestNum == N) and (Res == inProgress) ->
+            io:fwrite("ignoring duplicate request, still in progress~n"),
+            {next_state, master, State};
+        _ ->
+            io:fwrite("new request received~n"),
+            NewClientsTable = dict:store(Client, {RequestNum, inProgress}, ClientsTable),
+            NewLog = [Op|Log],
+            NewOpNumber = OpNumber + 1,
+            sendToReplicas(MasterNode, AllNodes, {prepare, ViewNumber, Op, OpNumber, CommitNumber}),
+            {next_state, master, State#{ clientsTable := NewClientsTable, 
+                log := NewLog, opNumber := NewOpNumber } }
+    end.
 
 replica({clientRequest, Client, _, _}, State) ->
     io:fwrite("Got client request as replica~n"),
     MasterNode = maps:get(masterNode, State),
-    Client ! {notMaster, MasterNode },
+    sendToClient(Client, {notMaster, MasterNode }),
+    {next_state, replica, State};
+
+replica({prepare, MasterViewNumber, _, _, _}, #{ viewNumber := ViewNumber} = State)
+    when MasterViewNumber < ViewNumber ->
+    io:fwrite("ignoring prepare from old view~n"),
+    {next_state, replica, State};
+
+replica({prepare, MasterViewNumber, Op, MasterOpNumber, MasterCommitNumber}, State) ->
+    io:fwrite("got prepare request~n"),
     {next_state, replica, State}.
 
-stop() ->
-    gen_fsm:send_all_state_event(vr, stop).
+
+stop(NodeName) ->
+    gen_fsm:send_all_state_event(NodeName, stop).
 
 handle_event(stop, _, State) ->
     {stop, normal, State}.
@@ -46,7 +92,7 @@ test() ->
     Nodes = [node1, node2],
     startNode(node1, node1, Nodes, Commit),
     startNode(node2, node1, Nodes, Commit),
-    startPrepare(node2, self(), 1, hello),
+    startPrepare(node1, self(), 1, hello),
     receive
         Result -> Result
     after
