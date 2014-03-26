43d406e1c630b8678223ed3140e7411a965ae500
Daniel King <daniel.zidan.king@gmail.com>
2014-03-26 14:41:15 -0400

    Randomize stopped node

--- tests/impl/stopnode.sh
+++ tests/impl/stopnode.sh
@@ -10,11 +10,9 @@ if [ "$1" = "primary" ]; then
     echo "Killing primary is unimplemented"
     exit 1;
 else
-    # i=`expr $RANDOM % $N + 1`
-    i=1
+    i=`expr $RANDOM % $N + 1`
     echo "$i" >> stoppednodes
     echo "about to kill n${i}@localhost"
-    # erl -sname "controller@localhost" -eval "rpc:block_call(n${i}@localhost,phat,stop,[])"
     TEMPFILE=$(mktemp /tmp/phat_escript.XXXXX)
     if [ 0 -ne $? ]; then
         echoerr "Could not create a temporary file, cannot complete"
