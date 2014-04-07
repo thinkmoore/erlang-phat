3dfb2c51bbbabacb0948f3d6c18321d1745b40ef
Daniel King <daniel.zidan.king@gmail.com>
2014-04-07 11:20:26 -0400

    switch to bash

--- tests/test1.sh
+++ tests/test1.sh
@@ -12,22 +12,22 @@ WORKAREA=$3
 function timer_total() {
     for i in `seq 1 2` # repeat 100
     do
-        sh ${IMPL}/stopnode.sh replica $N $WORKAREA
+        bash ${IMPL}/stopnode.sh replica $N $WORKAREA
         for i in `seq 1 10` # repeat 10
         do
             for i in `seq 1 5` # do 5 in parallel
             do
-                sh ${IMPL}/do.sh createfile 15 $N $WORKAREA &
+                bash ${IMPL}/do.sh createfile 15 $N $WORKAREA &
             done
         done
-        sh ${IMPL}/revivenode.sh $N $WORKAREA
+        bash ${IMPL}/revivenode.sh $N $WORKAREA
     done
 }
 
 # executable portion
 
-sh ${IMPL}/initialize.sh $WORKAREA
-sh ${IMPL}/startnodes.sh $N $WORKAREA
+bash ${IMPL}/initialize.sh $WORKAREA
+bash ${IMPL}/startnodes.sh $N $WORKAREA
 time timer_total
 
-sh ${IMPL}/verify.sh $N $WORKAREA
+bash ${IMPL}/verify.sh $N $WORKAREA
