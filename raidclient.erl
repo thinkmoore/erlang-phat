283e7a5ca416c0aa4d70cc731a342db3f60a6ac5
Andrew Johnson <ajohnson@seas.harvard.edu>
2014-05-09 17:29:38 -0400

    Number of parts to split is a parameter, Files don't have to be evenly divisible by number parts

--- raidclient.erl
+++ raidclient.erl
@@ -3,14 +3,14 @@
 %-define(NODEBUG, true). %% comment out for debugging messages
 -include_lib("eunit/include/eunit.hrl").
 -export([start_link/1,init/1,handle_call/3,call/1,stop/0,handle_cast/2,terminate/2]).
--export([code_change/3,handle_info/2]).
+-export([code_change/3,handle_info/2,xor_all/1,binary_bxor/2]).
 
-start_link(Nodes) ->
-    gen_server:start_link({local,raid},raidclient,Nodes,[]).
+start_link([Nodes,NumChunks]) ->
+    gen_server:start_link({local,raid},raidclient,[Nodes,NumChunks],[]).
 
-init(Nodes) ->
+init([Nodes,NumChunks]) ->
     client:start_link(Nodes),
-    {ok, #{ chunks => 3} }.
+    {ok, #{ chunks => NumChunks} }.
 
 call(Operation) ->
     gen_server:call(raid,Operation).
@@ -31,7 +31,7 @@ code_change(_,_,_) ->
 handle_info(_,_) ->
     {error,info_unsupported}.
 
-do_split_binary(Size, B) when byte_size(B) >= Size->
+do_split_binary(Size, B) when byte_size(B) >= 2*Size->
     {Chunk, Rest} = split_binary(B, Size),
     [ Chunk | do_split_binary(Size, Rest) ];
 do_split_binary(_, <<>>) ->
@@ -43,7 +43,16 @@ xor_all([Chunk]) ->
     Chunk;
 xor_all([H|Rest]) ->
     XorRest = xor_all(Rest),
-    list_to_binary([X bxor Y || X <- binary_to_list(XorRest), Y <- binary_to_list(H)]).    
+    binary_bxor(H, XorRest).
+
+binary_bxor(B1, B2) ->
+    S1 = size(B1)*8,
+    S2 = size(B2)*8,
+    <<I1:S1>> = B1,
+    <<I2:S2>> = B2,
+    I3 = I1 bxor I2,
+    S3 = max(S1, S2),
+    <<I3:S3>>.	       
 
 binary_to_chunks(B,G) ->
     Size = byte_size(B) div G,
@@ -73,13 +82,20 @@ get_chunks(Name,N) ->
 handle_call({store,Name,Data},_,State = #{ chunks := G}) ->
     Binary = binary:list_to_bin(Data),
     Parts = binary_to_chunks(Binary,G),
+    io:fwrite("~p~n", [Parts]),
     lists:foldl(do_chunk_helper(Name), 0, Parts),
     {reply, done, State};
 handle_call({get,Name},_,State = #{ chunks := G}) ->
     [Check|Parts] = lists:reverse(get_chunks(Name,G)),
-    io:fwrite("~p~n", [[Check|Parts]]),
+    ?debugFmt("~p~n", [[Check|Parts]]),
     Data = lists:foldl(fun (E,Acc) -> << Acc/binary, E/binary >> end, <<>>, Parts),
-    io:fwrite("~p~n", [binary_to_list(Data)]),
-    Check = xor_all(Parts),
+    ?debugFmt("~p~n", [binary_to_list(Data)]),
+    Check1 = xor_all(Parts),
+    if 
+	Check =:= Check1 ->
+	    io:fwrite("~p~n", ["Parity check passes!"]);
+	true -> 
+	    io:fwrite("~p~n", ["Parity check failed!"])
+    end,
     {reply, Data, State}.
     
--- testraid.erl
+++ testraid.erl
@@ -1,10 +1,10 @@
 -module(testraid).
 
--export([test1/0]).
+-export([test1/0,test2/0]).
 
 % c(testraid), testraid:test1().
 test1() ->
-    raidclient:start_link([n1@localhost,n2@localhost,n3@localhost]),
+    raidclient:start_link([[n1@localhost,n2@localhost,n3@localhost],3]),
     Clear = client:call({clear}),
     io:fwrite("clear result: ~p~n",[Clear]),
     Store = raidclient:call({store,"name1","foobarbaz"}),
@@ -20,4 +20,16 @@ test1() ->
     Get2 = raidclient:call({get,"name2"}),
     io:fwrite("get2 result: ~p~n",[Get2]),
     raidclient:stop().
+
+% c(testraid), testraid:test2().
+test2() ->
+    raidclient:start_link([[n1@localhost,n2@localhost,n3@localhost],4]),
+    Clear = client:call({clear}),
+    io:fwrite("clear result: ~p~n",[Clear]),
+    Store = raidclient:call({store,"name1","abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"}),
+    io:fwrite("store result: ~p~n",[Store]),
+    Get = raidclient:call({get,"name1"}),
+    io:fwrite("get result: ~p~n",[Get]),
+    raidclient:stop().
+
 
