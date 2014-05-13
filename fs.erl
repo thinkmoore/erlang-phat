e90538ac123c729c101b12e5fad521744bc4c893
Lucas Waye <wayetender@gmail.com>
2014-05-13 18:20:47 -0400

    fast

--- fs.erl
+++ fs.erl
@@ -11,10 +11,17 @@
 
 %% Disk latency simulation constants
 -define(MB_PER_CHAR, 1).
+%slow
 -define(DISK_WRITE_THRUPUT_MB_S, 55.0).
 -define(DISK_WRITE_ACCESS_TIME_S, 0.00032).
 -define(DISK_READ_THRUPUT_MB_S, 208.0).
 -define(DISK_READ_ACCESS_TIME_S, 0.00022).
+
+% fast
+% -define(DISK_WRITE_THRUPUT_MB_S, 482.0).
+% -define(DISK_WRITE_ACCESS_TIME_S, 0.00003).
+% -define(DISK_READ_THRUPUT_MB_S, 522.0).
+% -define(DISK_READ_ACCESS_TIME_S, 0.00003).
 
 %% Calls into the server
 getroot() ->
